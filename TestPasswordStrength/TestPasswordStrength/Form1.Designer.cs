d1f28432c2aa3e9d96cf0ed7f2f07f1b9e0c4724
bitf20m036 <146117175+bitf20m036@users.noreply.github.com>
2023-12-13 17:03:58 +0500

    Add files via upload

---
+++ TestPasswordStrength/TestPasswordStrength/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TestPasswordStrength
+{
+    partial class Form1 : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+        private TextBox txtPassword;
+        private Button btnCheckStrength;
+        private Label lblResult;
+        private ProgressBar progressBar;
+        private Label lblFeedback;
+        private Label lblPassword;
+        private Label lblStrength;
+        private Label lblProgressBar;
+        private Label lblFeedbackLabel;
+
+        private void InitializeComponent()
+        {
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnCheckStrength = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblStrength = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblProgressBar = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblFeedbackLabel = new System.Windows.Forms.Label();
+            this.lblFeedback = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(86, 44);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(201, 20);
+            this.txtPassword.TabIndex = 0;
+            // 
+            // btnCheckStrength
+            // 
+            this.btnCheckStrength.BackColor = System.Drawing.Color.White;
+            this.btnCheckStrength.Location = new System.Drawing.Point(125, 70);
+            this.btnCheckStrength.Name = "btnCheckStrength";
+            this.btnCheckStrength.Size = new System.Drawing.Size(116, 23);
+            this.btnCheckStrength.TabIndex = 6;
+            this.btnCheckStrength.Text = "Check Strength";
+            this.btnCheckStrength.UseVisualStyleBackColor = false;
+            this.btnCheckStrength.Click += new System.EventHandler(this.btnCheckStrength_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(28, 26);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(141, 15);
+            this.lblPassword.TabIndex = 7;
+            this.lblPassword.Text = "Enter your password:";
+            // 
+            // lblStrength
+            // 
+            this.lblStrength.AutoSize = true;
+            this.lblStrength.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStrength.Location = new System.Drawing.Point(28, 117);
+            this.lblStrength.Name = "lblStrength";
+            this.lblStrength.Size = new System.Drawing.Size(56, 15);
+            this.lblStrength.TabIndex = 4;
+            this.lblStrength.Text = "Result: ";
+            // 
+            // lblResult
+            // 
+            this.lblResult.Location = new System.Drawing.Point(38, 130);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(96, 26);
+            this.lblResult.TabIndex = 5;
+            // 
+            // lblProgressBar
+            // 
+            this.lblProgressBar.AutoSize = true;
+            this.lblProgressBar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgressBar.Location = new System.Drawing.Point(28, 156);
+            this.lblProgressBar.Name = "lblProgressBar";
+            this.lblProgressBar.Size = new System.Drawing.Size(91, 15);
+            this.lblProgressBar.TabIndex = 2;
+            this.lblProgressBar.Text = "Strength (%):";
+            // 
+            // progressBar
+            // 
+            this.progressBar.BackColor = System.Drawing.Color.White;
+            this.progressBar.Location = new System.Drawing.Point(125, 177);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(116, 20);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 3;
+            this.progressBar.Click += new System.EventHandler(this.progressBar_Click);
+            // 
+            // lblFeedbackLabel
+            // 
+            this.lblFeedbackLabel.AutoSize = true;
+            this.lblFeedbackLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFeedbackLabel.Location = new System.Drawing.Point(28, 207);
+            this.lblFeedbackLabel.Name = "lblFeedbackLabel";
+            this.lblFeedbackLabel.Size = new System.Drawing.Size(73, 15);
+            this.lblFeedbackLabel.TabIndex = 0;
+            this.lblFeedbackLabel.Text = "Feedback:";
+            // 
+            // lblFeedback
+            // 
+            this.lblFeedback.AutoSize = true;
+            this.lblFeedback.Location = new System.Drawing.Point(38, 220);
+            this.lblFeedback.Name = "lblFeedback";
+            this.lblFeedback.Size = new System.Drawing.Size(0, 13);
+            this.lblFeedback.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(350, 250);
+            this.Controls.Add(this.lblFeedbackLabel);
+            this.Controls.Add(this.lblFeedback);
+            this.Controls.Add(this.lblProgressBar);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lblStrength);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCheckStrength);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Name = "Form1";
+            this.Text = "Password Strength Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+    }
+}
---
+++ TestPasswordStrength/TestPasswordStrength/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TestPasswordStrength
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheckStrength_Click(object sender, EventArgs e)
+        {
+            string password = txtPassword.Text;
+            PasswordStrengthResult result = CheckPasswordStrength(password);
+
+            // Display detailed feedback
+            lblResult.Text = result.Strength;
+            lblFeedback.Text = result.Feedback;
+
+            // Update progress bar
+            progressBar.Value = result.Score;
+        }
+
+        private PasswordStrengthResult CheckPasswordStrength(string password)
+        {
+            // Add your criteria for password strength
+            bool lengthCriteria = password.Length >= 8;
+            bool uppercaseCriteria = password.Any(char.IsUpper);
+            bool lowercaseCriteria = password.Any(char.IsLower);
+            bool digitCriteria = password.Any(char.IsDigit);
+            bool specialCharCriteria = password.Any(c => !char.IsLetterOrDigit(c));
+
+            // Calculate the password score
+            int score = CalculatePasswordScore(lengthCriteria, uppercaseCriteria, lowercaseCriteria, digitCriteria, specialCharCriteria);
+
+            // Provide feedback based on the score
+            string feedback = GetPasswordFeedback(score);
+
+            // Determine the overall strength
+            string strength;
+            if (score >= 80)
+                strength = "Strong password";
+            else if (score >= 50)
+                strength = "Moderate password";
+            else
+                strength = "Weak password";
+
+            return new PasswordStrengthResult(strength, feedback, score);
+        }
+
+        private int CalculatePasswordScore(bool length, bool uppercase, bool lowercase, bool digit, bool specialChar)
+        {
+            int score = 0;
+
+            // Add scores based on criteria
+            score += length ? 20 : 0;
+            score += uppercase ? 20 : 0;
+            score += lowercase ? 20 : 0;
+            score += digit ? 20 : 0;
+            score += specialChar ? 20 : 0;
+
+            return score;
+        }
+
+        private string GetPasswordFeedback(int score)
+        {
+            // Provide feedback based on the score
+            if (score >= 80)
+                return "Excellent! Your password is very strong.";
+            else if (score >= 50)
+                return "Good effort! Your password is moderate. Consider adding more complexity.";
+            else
+                return "Weak password. Please choose a stronger password.";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class PasswordStrengthResult
+    {
+        public string Strength { get; }
+        public string Feedback { get; }
+        public int Score { get; }
+
+        public PasswordStrengthResult(string strength, string feedback, int score)
+        {
+            Strength = strength;
+            Feedback = feedback;
+            Score = score;
+        }
+    }
+}
